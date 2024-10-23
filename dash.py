789e54304b8005bdf9ed3e63c2c1573df90e24f6
Gustavo Benjamin <gustasbenjamin@gmail.com>
2024-10-22 22:13:28 -0300

    Gráficos implementados

--- dash.py
+++ dash.py
@@ -105,7 +105,124 @@ def graficos(df_selecionado):
         return
     
     # Criação dos gráficos
-    graf1, graf2, graf3, graf4 = st.tabs(['Gráfico de Barras', 'Gráfico de Linhas', 'Gráfico de Pizza', 'Gráfico de Dispersão'])
+    graf1, graf2, graf3, graf4, graf5 = st.tabs(['Gráfico de Barras', 
+                                                 'Gráfico de Linhas', 
+                                                 'Gráficos de Pizza', 
+                                                 'Gráfico de Dispersão',
+                                                 'Gráfico de Área'])
     
+    with graf1:
+        st.write('Gráfico de Barras')
+        
+        investimento = df_selecionado.groupby('marca').count()[['valor']].sort_values(by='valor', ascending=False)
+        # Agrupar pela marca e conta o numero de ocorrencias da coluna valor. Depois ordena o resultado de forma decrescente
+        
+        fig_valores = px.bar(investimento, # Dados que serão utilizados no gráfico
+                             x=investimento.index, 
+                             y='valor', orientation='v', 
+                             title='<b>Valores de Carros</b>', 
+                             color_discrete_sequence=['#0083b3']
+                             )
+        
+        st.plotly_chart(fig_valores, use_container_width=True)
     
-Home()
+    with graf2:
+        st.write('Gráfico de linhas')
+        dados = df_selecionado.groupby('marca').count()[['valor']]
+        
+        fig_valores2 = px.line(dados,
+                               x=dados.index,
+                               y='valor',
+                               title='<b>Valores por Marca</b>',
+                               color_discrete_sequence=['#0083b3']
+                               )
+        
+        st.plotly_chart(fig_valores2, use_container_width=True)
+    
+    with graf3:
+        st.write('Gráficos de Pizza')
+        dados2 = df_selecionado.groupby('marca').sum()[['valor']]
+        
+        fig_valores3 = px.pie(dados2,
+                              values='valor', # Valores que serão representados
+                              names=dados2.index, # Os nomes (marcas) que irão rotular
+                              title='<b>Distribuição de valores por marca</b>'
+                              )
+        
+        st.plotly_chart(fig_valores3, use_container_width=True)
+                
+        dados_vendas = df_selecionado.groupby('marca').sum()[['numero_vendas']]
+        
+        fig_valores3 = px.pie(dados_vendas,
+                              values='numero_vendas', # Valores que serão representados
+                              names=dados_vendas.index, # Os nomes (marcas) que irão rotular
+                              title='<b>Distribuição de vendas por marca</b>'
+                              )
+        
+        st.plotly_chart(fig_valores3, use_container_width=True)
+        
+        
+        
+    with graf4:
+        st.write('Gráfico de Dispersão')
+        dados3 = df_selecionado.melt(id_vars=['marca'], value_vars=['valor'])
+        
+        fig_valores4 = px.scatter(dados3,
+                                  x='marca',
+                                  y='value',
+                                  color='variable',
+                                  title='<b>Dispersão de valores por marca</b>'
+                                  )
+        
+        st.plotly_chart(fig_valores4, use_container_width=True)
+
+    with graf5:
+        st.write('Gráfico de Área')
+        dados4 = df_selecionado.melt(id_vars=['marca'], value_vars=['valor'])
+        
+        st.area_chart(dados4,
+                      x='marca',
+                      y='value',
+                      )
+ 
+        
+def barra_progresso():
+    valor_atual = df_selecionado['numero_vendas'].sum()
+    objetivo = 20000000
+    percentual = round((valor_atual/objetivo * 100))
+    
+    if percentual > 100:
+        st.subheader('Valores Atingidos!!!')
+    else:
+        st.write(f'Você tem {percentual}% de {objetivo}. Vai atrás paezão!')
+        mybar = st.progress(0)
+        for percentual_completo in range(percentual):
+            mybar.progress(percentual_completo + 1, text='Alvo %')
+            
+            
+# ********* MENU LATERAL *************
+def menu_lateral():
+    with st.sidebar:
+        selecionado = option_menu(menu_title='Menu', 
+                                options=['Home', 'Progresso'],
+                                icons=['house', 'eye'],
+                                menu_icon='cast',
+                                default_index=0
+                                )
+        
+    if selecionado == 'Home':
+        st.subheader(f'Página: {selecionado}')
+        Home()
+        graficos(df_selecionado)
+        
+    if selecionado == 'Progresso':
+        st.subheader(f'Página: {selecionado}')
+        barra_progresso()
+        graficos(df_selecionado)
+
+
+# ********* Ajustar o CSS ********
+
+
+
+menu_lateral()
